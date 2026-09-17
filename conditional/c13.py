a6c2a8d32ba7c4b62b58a8038f0686807c1fd5f2
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:15:13 +0530

    traffic signal

---
+++ conditional/c13.py
@@ -0,0 +1,10 @@
+print("traffic signal")
+color = input("enter color (red/yellow/green) : ")
+if color=="red":
+    print("stop")
+elif color=="yellow":
+    print("get ready")
+elif color=="green":
+    print("go")
+else:
+    print("enter valid color")
