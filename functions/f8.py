54f2dc48f22982ea263ffaf3c576bfa82f4c065c
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:45:11 +0530

    calculator

---
+++ functions/f8.py
@@ -0,0 +1,33 @@
+# calculator 
+
+print("ADDITION")
+def add(a,b):
+    print(a+b)
+
+a = int(input("enter 1st no : "))
+b = int(input("enter 2nd no : "))
+result = add(a,b)
+
+print("SUBTRACTION")
+def sub(a,b):
+    print(a-b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = sub(a,b)
+
+print("MULTIPLICATION")
+def mul(a,b):
+    print(a*b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = mul(a,b)
+
+print("DIVISION")
+def div(a,b):
+    print(a/b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = div(a,b)
