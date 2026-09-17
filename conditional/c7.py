8027fba5752ce92ae2c2b7975b77b6c8e254b8c6
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:11:06 +0530

    ticket(discount)

---
+++ conditional/c7.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age : "))
+ticket = 200
+if age<=12 :
+    ticket = ticket-(ticket*0.10)
+    print(f"your ticket price is {ticket}")
+else :
+    print(f"your ticket price is {ticket}")
