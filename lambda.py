19ca69e55c697e71e68e9fc9c3a7854a45c99005
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:46:08 +0530

    lambda function

---
+++ lambda.py
@@ -0,0 +1,11 @@
+# this is a long way 
+# def square(n):
+#     return n*n
+
+# this a smalll simple way ---lambda function
+# square = lambda n:n*n
+# print(square(5))
+
+add = lambda a,b:a+b
+print(add(5,30))
+
