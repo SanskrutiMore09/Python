4c9d16523968260063007cd17dbd204091e64ccd
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-17 07:33:44 +0530

    while loop problems

---
+++ loops.py
@@ -0,0 +1,75 @@
+# correct_pass ="some_pass"
+# not_found = True
+# while not_found : 
+#     string = input("enter a string : ")
+#     if string == correct_pass:
+#         not_found = False
+#     else:
+#         print("wrong password try again !!!")
+# print("password matched!!")
+
+# i=0
+# count = 0
+# while i<10 :
+#     print(i)
+#     i=i+1
+#     count=count+1
+# print(f"you executed while loop for  {count}")
+
+# i=2
+# count = 0
+# while i<10 :
+#     i=i+2
+#     count=count+1
+# print(f"even :  {count}")
+
+# break keyword.......
+# correct_pass ="some_pass"
+# while True: 
+#     string = input("enter a string : ")
+#     if string == correct_pass:
+#         break
+#     else:
+#         print("wrong password try again !!!")
+# print("password matched!!")
+
+# this has a bug 
+# i=0
+# while i < 10 :
+#     if i == 5 :
+#         continue
+#     print(i)
+#     i+=1
+
+# solution
+# i=0
+# while i < 10 :
+#     if i == 5 :
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+# num = int(input("enter the number : "))
+# i = 1
+# while i <= 10 :
+#     print(f"{num} * {i} = {num*i}") 
+#     i=i+1
+
+# prime no.
+# num = int(input("Enter the number: "))
+# if num > 1:
+#     i = 2
+#     while i < num:
+#         if num % i == 0:   
+#             print(f"{num} is not a prime number.")
+#             break
+#         i += 1
+#     else:
+#         print(f"{num} is a prime number.")
+# else:
+#     print(f"{num} is not a prime number.")
+
+
+
