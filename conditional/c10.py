42ecdd9597f2547435fc2319eac66cdc63116e7d
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:13:25 +0530

    greater between 3 no.

---
+++ conditional/c10.py
@@ -0,0 +1,17 @@
+a=int(input("enter your number : "))
+b=int(input("enter your number : "))
+c=int(input("enter your number : "))
+if a>b and a>c :
+    print("a is GREATER")
+elif b>a and b>c :
+    print (" b is GREATER")
+elif c>a and c>b :
+    print("c is GREATER")
+elif a==b or a==c : 
+    print ("two no.are equal")
+elif b==a or b==c : 
+    print ("two no.are equal")
+elif a==b or a==c : 
+    print ("two no. are equal")
+else :
+    print("invalid")
