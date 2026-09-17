16416e8dfdde1780f54aa514fcde712806698e4c
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:35:02 +0530

    created seperate files for all progarm and added this combined file in that folder

---
+++ functions/functions.py
@@ -0,0 +1,80 @@
+# def greet(name):
+#     print("hello",name)
+
+# greet("sanskruti")
+
+
+# def add(a,b):
+#     print(a+b)
+
+# add(5,3)
+# add(6,8)
+# add(10,9)
+
+# def pi():
+#     return 3.14
+# pi()
+
+# # two ways to return value 
+# # 1st 
+# print(pi())
+
+# # 2nd
+# pi=pi()
+# print(pi)
+
+# def add(a,b):
+#     return a+b
+# print(add(5,6))
+
+# def name(name="sanskruti"):
+#     print("hello",name)
+# name()
+
+# def name(name="sanskruti"):
+#     print("hello",name)
+# name("sakshi")
+
+# # wrong methods
+# def name():
+#     print("hello",name)
+# name()
+
+# def name(name):
+#     print("hello",name)
+# name()
+
+
+# calculator 
+
+print("ADDITION")
+def add(a,b):
+    print(a+b)
+
+a = int(input("enter 1st no : "))
+b = int(input("enter 2nd no : "))
+result = add(a,b)
+
+print("SUBTRACTION")
+def sub(a,b):
+    print(a-b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = sub(a,b)
+
+print("MULTIPLICATION")
+def mul(a,b):
+    print(a*b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = mul(a,b)
+
+print("DIVISION")
+def div(a,b):
+    print(a/b)
+
+a= int(input("enter 1st no : "))
+b= int(input("enter 2nd no : "))
+result = div(a,b)
