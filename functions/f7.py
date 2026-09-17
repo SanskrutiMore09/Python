241a781827b501d052a6e98140d45ed03f6faf66
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 01:43:52 +0530

    defualt argument and wrong methods

---
+++ functions/f7.py
@@ -0,0 +1,12 @@
+def name(name="sanskruti"):
+    print("hello",name)
+name("sakshi")
+
+# wrong methods
+def name():
+    print("hello",name)
+name()
+
+def name(name):
+    print("hello",name)
+name()
