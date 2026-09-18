beea4654f90565539c82009e18a457ad71896aa1
SanskrutiMore09 <sanskrutimore0913@gmail.com>
2026-09-18 23:56:04 +0530

    Program to count even and odd numbers in a list  using  loops

---
+++ loops/even_odd.py
@@ -0,0 +1,16 @@
+# Program to count even and odd numbers in a list
+
+numbers = [12, 7, 5, 64, 14, 9, 22]
+
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Numbers:", numbers)
+print("Even count:", even_count)
+print("Odd count:", odd_count)
